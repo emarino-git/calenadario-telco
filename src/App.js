d47b5a518ee25c69c9035fb8775ec4154f7897ca
emarino-git <82189750+emarino-git@users.noreply.github.com>
2022-06-22 04:53:12 +0000

    Cleaning code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import CalendarioSemanal from './components/CalendarioSemanal'
 import { MyCalendar } from './components/Calendar/Calendar' 
 
 function App() {
   return (
     <div className="App">
-      {/* <CalendarioSemanal /> */}
       <MyCalendar />
     </div>
   );
